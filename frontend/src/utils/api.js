a32c09454d3c11e11ff743a12493a94df5387968
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-09-26 22:41:36 +0530

    Update api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+
 const api = axios.create({
   baseURL: "http://localhost:5000/api", // Adjust this to your backend URL
   headers: {
